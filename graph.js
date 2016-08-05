2f0f9e576360f3105074a1ad0a34e12d390cdfcc
Chris <crmackay@gmail.com>
2016-08-05 18:27:22 -0400

    show only the last few hours

--- graph.js
+++ graph.js
@@ -1,8 +1,9 @@
 function plot(csvData, location){
     firstrows = csvData.split("\r\n")
+    lastHours = firstrows.slice(firstrows.length - 5000,firstrows.length)
     secondrows = [["time", "temp"]]
-    for(var i = 0; i< firstrows.length; i++){
-        var next = firstrows[i].split(",")
+    for(var i = 0; i< lastHours.length; i++){
+        var next = lastHours[i].split(",")
 
         secondrows.push([parseInt(next[0]), parseFloat(next[1])])
     }
