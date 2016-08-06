c12ce8846e77be7057845d0a06214b51e8f1524d
Chris <crmackay@gmail.com>
2016-08-05 23:50:53 -0400

    added current time

--- graph.js
+++ graph.js
@@ -2,7 +2,7 @@ function plot(csvData, location){
     var firstrows = csvData.split("\r\n")
     var lastHours = firstrows.slice(firstrows.length - 5000,firstrows.length)
     var condensed = []
-    var displayTemp = ""
+    var lastTempIndex = 0
     for (var i = 0; i< lastHours.length; i++) {
 
         var currTemp = lastHours[i].split(",")[1]
@@ -16,12 +16,14 @@ function plot(csvData, location){
             }
         }
         if (currTemp !== undefined) {
-            displayTemp = currTemp
+            lastTempIndex = i
         }
     }
-
+    var date = new Date(parseInt(lastHours[lastTempIndex].split(",")[0])*1000);
+    var currTime = date.toLocaleString()
+    var currTemp = lastHours[lastTempIndex].split(",")[1]
     // insert current temp
-    document.getElementById("freezer1-current-temp").innerHTML = displayTemp + "&deg;C"
+    document.getElementById("freezer1-current-temp").innerHTML = currTemp + "&deg; C <br/> Recorded at: &nbsp; &nbsp; &nbsp;" + currTime
 
     secondrows = [["time", "temp"]]
     for(var i = 0; i< condensed.length; i++){
